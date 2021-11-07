e8243f9e50ff4f6a5385c9140b530fe70f3ec2a5
edwinPato <edwin.p.a.pato@gmail.com>
2021-11-07 02:04:17 -0400

    Adding request files for common use, and .features files for user feature

---
+++ src/common/api/http.request.manager.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import CredentialsManager from '../../common/api/credentials.manager'
+
+class HttpRequestManager{
+
+    static makeRequest(verb, uri, data = '', authCredentials = 'AdminCredentials') {
+        let instance = CredentialsManager.getCredentials(authCredentials);
+
+        switch(verb) {
+            case "GET":
+                return instance.get(instance.defaults.baseURL + "/wp-json/" + uri, instance.defaults.auth);
+            case "POST":
+                return instance.post(instance.defautls.baseURL + "/wp-json/" + uri, data, instance.defaults.auth)
+            case "DELETE":   
+                return instance.delete(instace.defaults.baseURL + "/wp-json/" + uri, data, instance.defaults.auth) 
+        }
+    }
+}
+
+export default HttpRequestManager;
