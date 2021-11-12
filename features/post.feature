7a074da4541470bc3eb9eb0569d701b92f2b03d8
Javier Choque <javier.dreivaj@gmail.com>
2021-11-12 07:28:35 -0400

    Fixed ambiguos

--- features/post.feature
+++ features/post.feature
@@ -3,35 +3,35 @@ Feature: Crud post
 @Posts-Crud @Posts-create
 Scenario: CREATE WITH ALL REQUIRED FIELDS IS SUCCEFUL
 
-Given user wants to create a post with the following attributes
+Given User wants to create a post with the following attributes
     | payload | POST |
-When user save the new post a POST request to wp/v2/posts endpoint
-Then the save Created with code 201
+When User save the new post a POST request to wp/v2/posts endpoint
+Then The save Created with code 201
 
 @Posts-Crud @Posts-read
 Scenario: The endpoint should be able to retrieve the collection of all posts
-    Given A valid credentials
+    Given User with valid credentials
     When Executes a GET request to wp/v2/posts endpoint
-    Then The status code should be 200 OK
+    Then The post status code should be 200 OK
 
 @Posts-Crud @Posts-readById
 Scenario: The endpoint should be able to retrieve the collection of all posts by id post
-    Given A valid credentials
+    Given A Valid credentials.
     When Executes a GET request to wp/v2/posts/{id} endpoint
-    Then The status code should be 200 OK
+    Then The post status code should be 200 OK
 
 @Posts-Crud @Posts-update
 Scenario: UPDATE WITH ALL REQUIRED FIELDS IS SUCCEFUL
 
-Given user wants to update a post with the required attributes
+Given User wants to update a post with the required attributes
     | payload | PUT |
-When user update the post a POST request to wp/v2/posts/{id} endpoint
-Then the post is update OK with code 200
+When User update the post a POST request to wp/v2/posts/{id} endpoint
+Then The post will be update OK with code 200
 
 @Posts-Crud @Posts-delete
 Scenario: DELETE WITH ALL REQUIRED FIELDS IS SUCCEFUL
 
-Given user wants to delete a post with an identifier
+Given User wants to delete a post with an identifier
     | payload | DELETE |
-When user delete the post a DELETE request to wp/v2/posts/{id} endpoint
-Then the post is delete OK with code 200
+When User delete the post a DELETE request to wp/v2/posts/{id} endpoint
+Then The post will be delete OK with code 200
--- features/support/steps/steps.post.crud.js
+++ features/support/steps/steps.post.crud.js
@@ -10,12 +10,12 @@ let _endpoint = '';
 
 /*Created*/
 
-Given(/^user wants to create a post with the following attributes$/, function (dataTable) {
+Given(/^User wants to create a post with the following attributes$/, function (dataTable) {
     data = payloads.Valid[dataTable.rowsHash().payload]
     validCredentials = true
 });
 
-When(/^user save the new post a (.*) request to (.*) endpoint$/, async function (verb, endpoint) {
+When(/^User save the new post a (.*) request to (.*) endpoint$/, async function (verb, endpoint) {
     await HttpRequestManager.makeRequest(verb, endpoint, data, "AdminCredentials", "http://192.168.33.80:8080")
     .then(function (response) {
         _response = response;
@@ -26,14 +26,14 @@ When(/^user save the new post a (.*) request to (.*) endpoint$/, async function 
     })
 });
 
-Then(/^the save (.*) with code (\d+)$/, function (statusText, statusCode) {
+Then(/^The save (.*) with code (\d+)$/, function (statusText, statusCode) {
     expect(_response.status).to.equal(statusCode)
     expect(_response.statusText).to.equal(statusText)
 });
 
 /* READ */
 
-Given(/^A valid credentials$/, function () {
+Given(/^User with valid credentials$/, function () {
     validCredentials = true
 })
 
@@ -49,19 +49,19 @@ When(/^Executes a (.*) request to (.*) endpoint$/, async function (verb, endpoin
     })
 })
 
-Then(/^The status code should be (\d+) (.*)$/, function (statusCode, statusText) {
+Then(/^The post status code should be (\d+) (.*)$/, function (statusCode, statusText) {
     expect(_response.status).to.equal(statusCode)
     expect(_response.statusText).to.equal(statusText)
 })
 
 /*Update*/
 
-Given(/^user wants to update a post with the required attributes$/, function (dataTable) {
+Given(/^User wants to update a post with the required attributes$/, function (dataTable) {
     data = payloads.Valid[dataTable.rowsHash().payload]
     validCredentials = true
 });
 
-When(/^user update the post a (.*) request to (.*) endpoint$/, async function (verb, endpoint) {
+When(/^User update the post a (.*) request to (.*) endpoint$/, async function (verb, endpoint) {
     _endpoint = endpoint.replace('{id}', this.id)
     await HttpRequestManager.makeRequest(verb, _endpoint, data, "AdminCredentials", "http://192.168.33.80:8080")
     .then(function (response) {
@@ -73,18 +73,18 @@ When(/^user update the post a (.*) request to (.*) endpoint$/, async function (v
     })
 });
        
-Then(/^the post is update (.*) with code (\d+)$/, function (statusText, statusCode) {
+Then(/^The post will be update (.*) with code (\d+)$/, function (statusText, statusCode) {
     expect(_response.status).to.equal(statusCode)
     expect(_response.statusText).to.equal(statusText)
 });
 
 /*Delete*/
 
-Given(/^user wants to delete a post with an identifier$/, function (dataTable) {
+Given(/^User wants to delete a post with an identifier$/, function (dataTable) {
     validCredentials = true
 });
 
-When(/^user delete the post a (.*) request to (.*) endpoint$/, async function (verb, endpoint) {
+When(/^User delete the post a (.*) request to (.*) endpoint$/, async function (verb, endpoint) {
     _endpoint = endpoint.replace('{id}', this.id)
     await HttpRequestManager.makeRequest(verb, _endpoint, '', "AdminCredentials", "http://192.168.33.80:8080")
     .then(function (response) {
@@ -96,7 +96,7 @@ When(/^user delete the post a (.*) request to (.*) endpoint$/, async function (v
     })
 });
        
-Then(/^the post is delete (.*) with code (\d+)$/, function (statusText, statusCode) {
+Then(/^The post is delete (.*) with code (\d+)$/, function (statusText, statusCode) {
     expect(_response.status).to.equal(statusCode)
     expect(_response.statusText).to.equal(statusText)
 });
