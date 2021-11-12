0d18cfc4ce4f715e97e8fce6c91a8b70f5c8efd5
jchoque87 <80497096+jchoque87@users.noreply.github.com>
2021-11-12 13:54:57 -0400

    Develop post (#5)
    * Add post feature
    
    * Add post feature GET
    
    * Add Crud for Post
    
    * Fix Scenario
    
    * Add negative test
    
    * Add Changes
    
    * Fixed ambiguos

--- src/common/api/http.request.manager.js
+++ src/common/api/http.request.manager.js
@@ -3,12 +3,14 @@ import CredentialsManager from '../../common/api/credentials.manager'
 
 class HttpRequestManager{
 
-    static makeRequest(verb, uri, data = '', authCredentials = 'AdminCredentials') {
-        let instance = axios.create(CredentialsManager.getCredentials(authCredentials));
+    static makeRequest(verb, uri, data = '', authCredentials = 'AdminCredentials', baseURL=null) {
+
+        let instance = axios.create(CredentialsManager.getCredentials(authCredentials, baseURL));
+        let endpoint = instance.defaults.baseURL + "/wp-json/" + uri;
 
         switch(verb) {
             case "GET":
-                return instance.get(instance.defaults.baseURL + "/wp-json/" + uri, instance.defaults.auth);
+                return instance.get(endpoint, instance.defaults.auth);
             case "POST":
                 return instance.post(instance.defaults.baseURL + "/wp-json/" + uri, data, instance.defaults.auth);
             case "DELETE": 
